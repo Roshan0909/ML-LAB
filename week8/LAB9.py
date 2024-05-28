b78902603734028e22d41bbd2b9342028248cc98
nagaroshan22aib06 <126764223+nagaroshan22aib06@users.noreply.github.com>
2024-05-28 22:19:38 +0530

    Update LAB9.py

--- week8/LAB9.py
+++ week8/LAB9.py
@@ -1,18 +1,24 @@
+# iris_knn.py
+
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-dataset=load_iris()
-#print(dataset)
-X_train,X_test,y_train,y_test=train_test_split(dataset["data"],dataset["target"],random_state=0)
+# Load the Iris dataset
+dataset = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(dataset["data"], dataset["target"], random_state=0)
 
-kn=KNeighborsClassifier(n_neighbors=1)
-kn.fit(X_train,y_train)
+# Initialize and train the k-nearest neighbors classifier
+kn = KNeighborsClassifier(n_neighbors=1)
+kn.fit(X_train, y_train)
 
+# Make predictions on the test set and print results
 for i in range(len(X_test)):
-    x=X_test[i]
-    x_new=np.array([x])
-    prediction=kn.predict(x_new)
-    print("TARGET=",y_test[i],dataset["target_names"][y_test[i]],"PREDICTED=",prediction,dataset["target_names"][prediction])
-print(kn.score(X_test,y_test))
+    x = X_test[i]
+    x_new = np.array([x])
+    prediction = kn.predict(x_new)
+    print("TARGET=", y_test[i], dataset["target_names"][y_test[i]], "PREDICTED=", prediction, dataset["target_names"][prediction])
+
+# Print the accuracy of the classifier
+print(kn.score(X_test, y_test))
